adfd0ef84815eec2958fc47fc3c05b582adba054
Rodrigo Lima <rodrigo_rossetti_lima@hotmail.com>
2020-03-20 23:10:26 -0300

    Aula 119

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const User = require("./User");
+const bcrypt = require('bcryptjs');
 
 router.get("/admin/users", (req, res) => {
-    res.send("Listagem de usuários");
+    User.findAll().then(users => {
+        res.render("admin/users/index", {
+            users: users
+        });
+    });
 });
 
 router.get("/admin/users/create", (req, res) => {
@@ -14,7 +19,23 @@ router.post("/users/create", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    res.json({email, password});
+    User.findOne({where: {email: email}}).then(user => {
+        if(user == undefined){
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+        
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect("/")
+            }).catch(erro => {
+                res.redirect("/")
+            });
+        } else {
+            res.redirect("/admin/users/create");
+        }
+    });
 });
 
 module.exports = router;
